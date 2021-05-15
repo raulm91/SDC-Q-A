a19974469d623b151f37a69a1522ebc3eb20ab20
Raul Martinez <raul91martinez@gmail.com>
2021-05-15 13:09:15 -0700

    newLoader

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017/questions', { useNewUrlParser: true 
 const db = mongoose.connection;
 
 app.get('loaderio-2df3fdbcdf44fa202ce4049648896499', (req, res) => {
-  res.sendFile(path.join(__dirname, './loaderio-2df3fdbcdf44fa202ce4049648896499.txt'));
+  res.sendFile('loaderio-2df3fdbcdf44fa202ce4049648896499');
 });
 
 //POST requests for questions and answers
